237f3e0cc33524141e2e5f42969a1581ce0548ee
lgvalent@gmail.com <lgvalent@gmail.com@25fcfc5a-bb47-0410-a2ba-a708640e2d6d>
2014-03-25 14:58:55 +0000

    Adaptação como IRunnable

---
+++ trunk/Financeiro/src/br/com/orionsoft/financeiro/gerenciador/process/ListarItemCustoProcessController.java
@@ -0,0 +1,51 @@
+package br.com.orionsoft.financeiro.gerenciador.process;
+
+import br.com.orionsoft.financeiro.gerenciador.entities.Conta;
+import br.com.orionsoft.financeiro.gerenciador.entities.ItemCusto;
+import br.com.orionsoft.monstrengo.core.process.IProcess;
+import br.com.orionsoft.monstrengo.core.process.IRunnableEntityProcessController;
+import br.com.orionsoft.monstrengo.core.process.ProcessException;
+import br.com.orionsoft.monstrengo.core.process.RunnableProcessControllerBasic;
+import br.com.orionsoft.monstrengo.crud.entity.EntityException;
+import br.com.orionsoft.monstrengo.crud.entity.IEntity;
+
+/**
+ * @author Lucio 20140325
+ */
+public class ListarItemCustoProcessController extends RunnableProcessControllerBasic implements IRunnableEntityProcessController {
+	
+    /** Informa para o gerenciador quais as entidades que são compatíveis com este controlador */
+	public static final Class<?>[] RUNNABLE_ENTITIES = {ItemCusto.class, Conta.class};
+	public Class<?>[] getRunnableEntities() {return RUNNABLE_ENTITIES;}
+
+	public Class<? extends IProcess> getProcessClass() {return ListarItemCustoProcess.class;}
+	
+	/**
+	 * @throws EntityException 
+	 * @see Consulte {@link IRunnableEntityProcessController.canRunWithEntity(IEntity)}
+	 */
+	public boolean canRunWithEntity(IEntity<?> entity) throws ProcessException {
+		/* Limpa a atual mensagem do controlador */
+		this.setMessage(null);
+
+		/* Podem ser adicionados aqui códigos de validação 
+		 * para verificar se os atuais valores das propriedades
+		 * da entidade permitem a execução deste processo sobre
+		 * a entidade. */
+//		try{
+//			if(entity.getInfo().getType() == Contrato.class){
+//				/* Verifica se o saldo em aberto permite quitar */
+//				if(false){
+//					this.setMessage(new BusinessMessage(ListarPosicaoContratoProcessController.class, "DESCRICAO_DA_VALIDAÇÂO"));
+//					return false;
+//				}
+//			}	
+//		}catch(BusinessException e){
+//			throw new ProcessException(e.getErrorList());
+//		}
+		
+		return true;	
+	}
+
+
+}
