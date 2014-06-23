d14daf5976bf568000ee8618b4fe476d085f3502
lgvalent <lgvalent@gmail.com>
2014-06-23 21:22:57 +0000

    Atualiza o valor do título de acordo com o valor que veio no arquivo de retorno do banco.

--- Financeiro/src/br/com/orionsoft/financeiro/documento/cobranca/titulo/banco748/Gerenciador748.java
+++ Financeiro/src/br/com/orionsoft/financeiro/documento/cobranca/titulo/banco748/Gerenciador748.java
@@ -833,9 +833,27 @@ public class Gerenciador748 extends GerenciadorBancoBasic
 											doc.setValorPago(detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_EFETIVAMENTE_PAGO));
 
 										try{
-											/* Subtitui o valor do titulo, pois o usuario pode ter pago um valor maioir no banco */ 
-											oTitulo.setValor(detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_TITULO));
-											doc.setMensagem("O valor do título foi modificado de R$ "+ titulo.getProperty(DocumentoTitulo.VALOR).getValue().getAsString() +"para R$" + detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_TITULO));
+											BigDecimal valorTituloArquivo = detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_TITULO); 
+											/* Se o banco informar o valor do título, então usa este valor como oficial */
+											if(valorTituloArquivo.doubleValue() != 0.00){
+												/* Subtitui o valor do titulo, pois o usuario pode ter pago um valor maior no banco */
+												doc.setMensagem("O valor do título foi modificado de R$ "+ titulo.getProperty(DocumentoTitulo.VALOR).getValue().getAsString() +"para R$" + valorTituloArquivo);
+												oTitulo.setValor(valorTituloArquivo);
+											}
+
+											/* Se o título estiver com valor ZERO, então usa o valor PAGO como o valor do título, subtraindo os juros, multa e etc */
+											if(oTitulo.getValor().doubleValue() == 0.00){
+												/* Subtitui o valor do titulo, pois o usuário pode ter pago um valor maior no banco */
+												BigDecimal valorPago = detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_EFETIVAMENTE_PAGO);
+												valorPago = valorPago.subtract(detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_JUROS));
+												valorPago = valorPago.subtract(detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_MULTA));
+												valorPago = valorPago.subtract(detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_DESCONTO_CONCEDIDO));
+												valorPago = valorPago.subtract(detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_ABATIMENTO_CONCEDIDO));
+												
+												doc.setMensagem("O valor do título foi modificado de R$ "+ titulo.getProperty(DocumentoTitulo.VALOR).getValue().getAsString() +"para R$" + valorPago);
+												oTitulo.setValor(valorPago);
+											}
+											
 											doc.setValorDocumento(oTitulo.getValor());
 										} catch (BusinessException e) {
 											log.debug("ERRO ao atualizar 'Titulo'- " + titulo.getPropertyValue(DocumentoTitulo.VALOR) + " -/- 'Arq Retorno'- " + detalhes.getAsBigDecimal(DetalheRetornoSemRegistro.VALOR_TITULO));
