77d4745eb733d43771ad9ce3fcc517b824342cee
lgvalent@gmail.com <lgvalent@gmail.com@25fcfc5a-bb47-0410-a2ba-a708640e2d6d>
2015-03-31 19:32:52 +0000

    Evita o cancelamento de lançamentos já cancelados.

--- trunk/Financeiro/src/br/com/orionsoft/financeiro/gerenciador/services/CancelarLancamentoService.java
+++ trunk/Financeiro/src/br/com/orionsoft/financeiro/gerenciador/services/CancelarLancamentoService.java
@@ -32,93 +32,99 @@ import br.com.orionsoft.monstrengo.crud.services.UtilsCrud;
  * @spring.property name="serviceManager" ref="ServiceManager"
  */
 public class CancelarLancamentoService extends ServiceBasic {
-    public static final String SERVICE_NAME = "CancelarLancamentoService";
-    
-    public static final String IN_DATA = "data";
-    public static final String IN_LANCAMENTO = "lancamento";
-    public static final String IN_DESCRICAO = "descricao";
+	public static final String SERVICE_NAME = "CancelarLancamentoService";
 
-    public String getServiceName() {
-        return SERVICE_NAME;
-    }
+	public static final String IN_DATA = "data";
+	public static final String IN_LANCAMENTO = "lancamento";
+	public static final String IN_DESCRICAO = "descricao";
 
-    public void execute(ServiceData serviceData) throws ServiceException {
-    	log.debug("::Iniciando a execução do serviço CancelarLancamentoService");
+	public String getServiceName() {
+		return SERVICE_NAME;
+	}
 
-        try {
-            log.debug("Preparando os argumentos");
-            
-            /* 
-             * Lê os parâmetros obrigatórios. 
-             */
-            Calendar inData = (Calendar) serviceData.getArgumentList().getProperty(IN_DATA);
-            Lancamento inLancamento = (Lancamento) serviceData.getArgumentList().getProperty(IN_LANCAMENTO);
-            String inDescricao = (String) serviceData.getArgumentList().getProperty(IN_DESCRICAO);
+	public void execute(ServiceData serviceData) throws ServiceException {
+		log.debug("::Iniciando a execução do serviço CancelarLancamentoService");
 
-            /*
-             * Falha se tiver saldo em aberto não maior que zero.
-             */
-            ServiceData sdSaldo = new ServiceData(CalcularSaldoAbertoLancamentoService.SERVICE_NAME, serviceData);
-            sdSaldo.getArgumentList().setProperty(CalcularSaldoAbertoLancamentoService.IN_LANCAMENTO, inLancamento);
-            this.getServiceManager().execute(sdSaldo);
-            BigDecimal saldo = (BigDecimal)sdSaldo.getFirstOutput();
-            /* Lucio 20141028: Para cancelar, não precisa ter saldo em aberto!!!*/
-//        	if (saldo.signum() == 0)
-//                throw new ServiceException(MessageList.create(CancelarLancamentoService.class, "FALHA_VALOR_ZERO"));
+		try {
+			log.debug("Preparando os argumentos");
 
-            /*
-             * Insere um LancamentoMovimento para este Lancamento.
-             */
-            ServiceData sd = new ServiceData(InserirLancamentoMovimentoService.SERVICE_NAME, serviceData);
-            sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_LANCAMENTO, inLancamento);
-            sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_DATA, inData);
-            sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_DATA_COMPENSACAO_OPT, inData);
-            sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_DESCRICAO, inDescricao);
-            sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_VALOR, saldo);
-            sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_CONTA, null);
-            sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_LANCAMENTO_MOVIMENTO_CATEGORIA, LancamentoMovimentoCategoria.CANCELADO);
-            this.getServiceManager().execute(sd);
-            LancamentoMovimento lancamentoMovimento = sd.getFirstOutput();
-            
-            /*
-             * Recarrega lançamento para atualização.
-             */
-            inLancamento = UtilsCrud.objectRetrieve(this.getServiceManager(), Lancamento.class, inLancamento.getId(), serviceData);
-            
-            /*
-             * Recalcula o saldo do lançamento.
-             */
-            sd = new ServiceData(CalcularSaldoAbertoLancamentoService.SERVICE_NAME, serviceData);
-            sd.getArgumentList().setProperty(CalcularSaldoAbertoLancamentoService.IN_LANCAMENTO, inLancamento);
-            this.getServiceManager().execute(sd);
-            saldo = sd.getFirstOutput();
-            
-            /*
-             * Atualiza o lançamento com o novo saldo.
-             */
-            inLancamento.setSaldo(saldo);
-            inLancamento.setLancamentoSituacao(LancamentoSituacao.CANCELADO);
-            UtilsCrud.objectUpdate(this.getServiceManager(), inLancamento, serviceData);
-            
-            /*
-             * Adiciona o LancamentoMovimento ao retorno do serviço.
-             */
-            lancamentoMovimento = UtilsCrud.objectRetrieve(this.getServiceManager(), LancamentoMovimento.class, lancamentoMovimento.getId(), serviceData);
-            serviceData.getOutputData().add(lancamentoMovimento);
+			/* 
+			 * Lê os parâmetros obrigatórios. 
+			 */
+			Calendar inData = (Calendar) serviceData.getArgumentList().getProperty(IN_DATA);
+			Lancamento inLancamento = (Lancamento) serviceData.getArgumentList().getProperty(IN_LANCAMENTO);
+			String inDescricao = (String) serviceData.getArgumentList().getProperty(IN_DESCRICAO);
 
-            /* 
-             * Adiconando a mensagem de sucesso 
-             */
-            this.addInfoMessage(serviceData, "SUCESSO");
-		
-            log.debug("::Fim da execução do serviço");
-        } catch (BusinessException e) {
-            log.fatal(e.getErrorList());
-            throw new ServiceException(e.getErrorList());
-        } catch (Exception e) {
-            log.fatal(e.getMessage());
-            throw new ServiceException(MessageList.createSingleInternalError(e));
-        }
-    }
+			/*
+			 * Falha se tiver saldo em aberto não maior que zero.
+			 */
+			ServiceData sdSaldo = new ServiceData(CalcularSaldoAbertoLancamentoService.SERVICE_NAME, serviceData);
+			sdSaldo.getArgumentList().setProperty(CalcularSaldoAbertoLancamentoService.IN_LANCAMENTO, inLancamento);
+			this.getServiceManager().execute(sdSaldo);
+			BigDecimal saldo = (BigDecimal)sdSaldo.getFirstOutput();
+			/* Lucio 20141028: Para cancelar, não precisa ter saldo em aberto!!!*/
+			//        	if (saldo.signum() == 0)
+			//                throw new ServiceException(MessageList.create(CancelarLancamentoService.class, "FALHA_VALOR_ZERO"));
+
+			/* Lucio 20150310: Evita o cancelamento de lançamento já cancelado */
+			if (inLancamento.getLancamentoSituacao() != LancamentoSituacao.CANCELADO){
+
+				/*
+				 * Insere um LancamentoMovimento para este Lancamento.
+				 */
+				ServiceData sd = new ServiceData(InserirLancamentoMovimentoService.SERVICE_NAME, serviceData);
+				sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_LANCAMENTO, inLancamento);
+				sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_DATA, inData);
+				sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_DATA_COMPENSACAO_OPT, inData);
+				sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_DESCRICAO, inDescricao);
+				sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_VALOR, saldo);
+				sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_CONTA, null);
+				sd.getArgumentList().setProperty(InserirLancamentoMovimentoService.IN_LANCAMENTO_MOVIMENTO_CATEGORIA, LancamentoMovimentoCategoria.CANCELADO);
+				this.getServiceManager().execute(sd);
+				LancamentoMovimento lancamentoMovimento = sd.getFirstOutput();
+
+				/*
+				 * Recarrega lançamento para atualização.
+				 */
+				inLancamento = UtilsCrud.objectRetrieve(this.getServiceManager(), Lancamento.class, inLancamento.getId(), serviceData);
+
+				/*
+				 * Recalcula o saldo do lançamento.
+				 */
+				sd = new ServiceData(CalcularSaldoAbertoLancamentoService.SERVICE_NAME, serviceData);
+				sd.getArgumentList().setProperty(CalcularSaldoAbertoLancamentoService.IN_LANCAMENTO, inLancamento);
+				this.getServiceManager().execute(sd);
+				saldo = sd.getFirstOutput();
+
+				/*
+				 * Atualiza o lançamento com o novo saldo.
+				 */
+				inLancamento.setSaldo(saldo);
+				inLancamento.setLancamentoSituacao(LancamentoSituacao.CANCELADO);
+				UtilsCrud.objectUpdate(this.getServiceManager(), inLancamento, serviceData);
+
+				/*
+				 * Adiciona o LancamentoMovimento ao retorno do serviço.
+				 */
+				lancamentoMovimento = UtilsCrud.objectRetrieve(this.getServiceManager(), LancamentoMovimento.class, lancamentoMovimento.getId(), serviceData);
+				serviceData.getOutputData().add(lancamentoMovimento);
+
+				/* 
+				 * Adiconando a mensagem de sucesso 
+				 */
+				this.addInfoMessage(serviceData, "SUCESSO");
+			}else{
+				this.addInfoMessage(serviceData, "JA_CANCELADO");
+				
+			}
+			log.debug("::Fim da execução do serviço");
+		} catch (BusinessException e) {
+			log.fatal(e.getErrorList());
+			throw new ServiceException(e.getErrorList());
+		} catch (Exception e) {
+			log.fatal(e.getMessage());
+			throw new ServiceException(MessageList.createSingleInternalError(e));
+		}
+	}
 
 }
