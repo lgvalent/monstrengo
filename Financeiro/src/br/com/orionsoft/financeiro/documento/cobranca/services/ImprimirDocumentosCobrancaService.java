ef8103d1b6dc067656b807a8be641b0a9a1901af
Lucio Valentin <lgvalentin@utfpr.edu.br>
2018-11-09 01:35:03 -0200

    Validação do endereço do e-mail antes da tentativa de envio do documento

--- Financeiro/src/br/com/orionsoft/financeiro/documento/cobranca/services/ImprimirDocumentosCobrancaService.java
+++ Financeiro/src/br/com/orionsoft/financeiro/documento/cobranca/services/ImprimirDocumentosCobrancaService.java
@@ -45,7 +45,7 @@ import br.com.orionsoft.monstrengo.mail.services.SendMailService;
  * @spring.property name="transactional" value="true"
  */
 public class ImprimirDocumentosCobrancaService extends DocumentoCobrancaServiceBasic {
-	
+
 	public static final String SERVICE_NAME = "ImprimirDocumentosCobrancaService";
 
 	public static final String IN_DOCUMENTO_OPT = "documento";
@@ -56,47 +56,47 @@ public class ImprimirDocumentosCobrancaService extends DocumentoCobrancaServiceB
 	public static final String IN_INSTRUCOES_ADICIONAIS_OPT = "instrucoesAdicionais"; 
 	public static final String IN_INPUT_STREAM_IMAGEM_OPT = "inInputStreamImagem";
 
-	
+
 	public String getServiceName() {
 		return SERVICE_NAME;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public void execute(ServiceData serviceData) throws ServiceException {
 		log.debug("::Iniciando execução do servico ImprimirDocumentosCobrancaService");
-		
+
 		try{
 			log.debug("Preparando os argumentos");
 			Boolean inEnviarEMail = (serviceData.getArgumentList()
 					.containsProperty(IN_ENVIAR_EMAIL_OPT) ? (Boolean) serviceData
-					.getArgumentList().getProperty(IN_ENVIAR_EMAIL_OPT)
-					: false);
+							.getArgumentList().getProperty(IN_ENVIAR_EMAIL_OPT)
+							: false);
 
 			OutputStream inOutputStream = (serviceData.getArgumentList()
 					.containsProperty(IN_OUTPUT_STREAM_OPT) ? (OutputStream) serviceData
-					.getArgumentList().getProperty(IN_OUTPUT_STREAM_OPT) : null);
-			
+							.getArgumentList().getProperty(IN_OUTPUT_STREAM_OPT) : null);
+
 			int inPrinterIndex = (serviceData.getArgumentList()
 					.containsProperty(IN_PRINTER_INDEX_OPT) ? (Integer) serviceData
-					.getArgumentList().getProperty(IN_PRINTER_INDEX_OPT)
-					: PrintUtils.PRINTER_INDEX_NO_PRINT);
+							.getArgumentList().getProperty(IN_PRINTER_INDEX_OPT)
+							: PrintUtils.PRINTER_INDEX_NO_PRINT);
 
 			String inInstrucoesAdicionais = (serviceData.getArgumentList()
 					.containsProperty(IN_INSTRUCOES_ADICIONAIS_OPT) ? (String) serviceData
-					.getArgumentList()
-					.getProperty(IN_INSTRUCOES_ADICIONAIS_OPT) : "");
+							.getArgumentList()
+							.getProperty(IN_INSTRUCOES_ADICIONAIS_OPT) : "");
 
 			IEntity<? extends DocumentoCobranca> inDocumento = (serviceData.getArgumentList()
 					.containsProperty(IN_DOCUMENTO_OPT) ? (IEntity<? extends DocumentoCobranca>) serviceData
-					.getArgumentList().getProperty(IN_DOCUMENTO_OPT) : null);
+							.getArgumentList().getProperty(IN_DOCUMENTO_OPT) : null);
 			List<DocumentoCobrancaBean> inDocumentosBean = (serviceData
 					.getArgumentList().containsProperty(IN_DOCUMENTO_BEAN_LIST) ? (List<DocumentoCobrancaBean>) serviceData
-					.getArgumentList().getProperty(IN_DOCUMENTO_BEAN_LIST)
-					: null);
+							.getArgumentList().getProperty(IN_DOCUMENTO_BEAN_LIST)
+							: null);
 			InputStream inInputStreamImagem = (serviceData.getArgumentList()
 					.containsProperty(IN_INPUT_STREAM_IMAGEM_OPT) ? (InputStream) serviceData
-					.getArgumentList().getProperty(IN_INPUT_STREAM_IMAGEM_OPT)
-					: null);
+							.getArgumentList().getProperty(IN_INPUT_STREAM_IMAGEM_OPT)
+							: null);
 
 			/* Lucio 20110517 Identifica se tem documentos de diferentes tipos para lançar uma exceção */
 			if(inDocumentosBean != null){
@@ -105,18 +105,18 @@ public class ImprimirDocumentosCobrancaService extends DocumentoCobrancaServiceB
 					if(bean.isChecked()){
 						IEntity<? extends DocumentoCobranca> documentoTemp = bean.isTemDocumentoOriginal()?bean.getDocumentoOriginal():UtilsCrud.retrieve(this.getServiceManager(), DocumentoCobranca.class, bean.getId(), serviceData);
 						String gerenciadorAtual =  documentoTemp.getObject().getConvenioCobranca().getNomeGerenciadorDocumento();
-						
+
 						if((gerenciadorAnterior != null) && !(gerenciadorAnterior.equals(gerenciadorAtual))){
 							log.debug("Documentos de diferentes gerenciadores foram selecionados ");
-				            throw new ServiceException(MessageList.create(ImprimirDocumentosCobrancaService.class, "ERRO_DIFERENTES_GERENCIADORES"));
-							
+							throw new ServiceException(MessageList.create(ImprimirDocumentosCobrancaService.class, "ERRO_DIFERENTES_GERENCIADORES"));
+
 						}else{
 							gerenciadorAnterior = gerenciadorAtual;
 						}
 					}
 				}
 			}
-			
+
 
 			log.debug("Obtendo junto ao ProvedorDocumentoCobranca o gerenciador responsável");
 			/* TODO Se um elemento seleciona nao for do mesmo tipo do primeiro elemento da lista, dará um erro
@@ -138,9 +138,9 @@ public class ImprimirDocumentosCobrancaService extends DocumentoCobrancaServiceB
 				gerenciador = this.retrieveGerenciadorPorDocumento((DocumentoCobranca) documentoTemp.getObject());
 			}else{
 				log.debug("Não foi passado um documento ou a lista está vazia");
-	            throw new ServiceException(MessageList.create(ImprimirDocumentosCobrancaService.class, "ERRO_NENHUM_DOCUMENTO"));
+				throw new ServiceException(MessageList.create(ImprimirDocumentosCobrancaService.class, "ERRO_NENHUM_DOCUMENTO"));
 			}
-			
+
 			log.debug("Executando o método de impressão avulsa do gerenciador");
 			if(inDocumento!=null){
 				if(inEnviarEMail){
@@ -167,45 +167,48 @@ public class ImprimirDocumentosCobrancaService extends DocumentoCobrancaServiceB
 
 			log.debug("Adicionando a mensagem de sucesso");
 			this.addInfoMessage(serviceData, "IMPRESSAO_SUCESSO");
-			
+
 		} catch (BusinessException e) {
 			log.fatal(e.getErrorList());
 			/* O Serviço não precisa adicionar mensagem local. O Manager já indica qual srv falhou e os parâmetros. */
 			throw new ServiceException(e.getErrorList());
 		} catch (Exception e) {
 			log.fatal(e.getMessage());
-			
+
 			/* Indica que o serviço falhou por causa de uma exceção do hibernate. */
 			throw new ServiceException(MessageList.createSingleInternalError(e));
 		}
-		
+
 	}
-	
+
 	private void enviarEMail(ByteArrayOutputStream pdfOutputStream, IEntity<? extends DocumentoCobranca> documento, ServiceData serviceData) throws BusinessException{
 		/* TODO Lucio 20181029	Se der erro ao enviar um email, tem que ver como tratar transacionalmente!!! Os emails errados vão retornar como MSG para o e-mail origem */
 		if(documento.getObject().getDocumentoCobrancaCategoria().getContaEMail() == null)
 			throw new ServiceException(MessageList.create(ImprimirDocumentosCobrancaService.class, "CONTA_EMAIL_NAO_DEFINIDA", documento.getObject().getDocumentoCobrancaCategoria(), documento.getObject().getDocumentoCobrancaCategoria().getId()));
-		
+
 		if(StringUtils.isBlank(documento.getObject().getDocumentoCobrancaCategoria().getMensagemEMail()))
 			throw new ServiceException(MessageList.create(ImprimirDocumentosCobrancaService.class, "MENSAGEM_EMAIL_NAO_DEFINIDA", documento.getObject().getDocumentoCobrancaCategoria(), documento.getObject().getDocumentoCobrancaCategoria().getId()));
-		
+
 		String mensagemHtml = "";
 		if (!documento.getProperty(DocumentoCobranca.DOCUMENTO_COBRANCA_CATEGORIA).getValue().isValueNull())
 			mensagemHtml = DocumentParserCrudExpression.parseString(documento.getObject().getDocumentoCobrancaCategoria().getMensagemEMail(), documento, this.getServiceManager().getEntityManager());
-		
+
 		String email = documento.getObject().getDocumentoCobrancaCategoria().getContaEMail().getSenderMail();
-		String assunto = "[Empresa sem e-mail cadastrado]Documento de Cobrança: " + documento.toString();
+		String assunto = "[Empresa com E-Mail inválido ou inexistente] Documento de Cobrança: " + documento.toString();
 		if(StringUtils.isNotBlank(documento.getObject().getContrato().getPessoa().getEmail())){
-				email = documento.getObject().getContrato().getPessoa().getEmail().split(";")[0];
+			String emailTemp = documento.getObject().getContrato().getPessoa().getEmail().split(";")[0];
+			if(!SendMailService.validateEMail(emailTemp)){
+				email = emailTemp;
 				assunto = "Documento de Cobrança: " + documento.toString();
+			}
 		}
-				
+
 		ServiceData service = new ServiceData(SendMailService.SERVICE_NAME, serviceData);
 		service.getArgumentList().setProperty(SendMailService.IN_EMAIL_ACCOUNT_OPT, documento.getObject().getDocumentoCobrancaCategoria().getContaEMail());
 		service.getArgumentList().setProperty(SendMailService.IN_MESSAGE, mensagemHtml);
 		service.getArgumentList().setProperty(SendMailService.IN_RECIPIENT_OPT, email);
 		service.getArgumentList().setProperty(SendMailService.IN_SUBJECT, assunto);
-		
+
 		// Test MimeBodyPart attachment
 		MimeBodyPart bodyPart = new MimeBodyPart();
 		try {
@@ -215,13 +218,13 @@ public class ImprimirDocumentosCobrancaService extends DocumentoCobrancaServiceB
 			bodyPart.setDisposition(MimeBodyPart.ATTACHMENT);
 			bodyPart.setContent(pdfOutputStream.toByteArray(), "application/pdf");
 			bodyList.add(bodyPart);
-			
+
 			service.getArgumentList().setProperty(SendMailService.IN_MIME_BODY_PART_LIST_OPT, bodyList);
-			
+
 			this.getServiceManager().execute(service);
 		} catch (Exception e) {
 			log.fatal(e.getMessage());
-			
+
 			/* Indica que o serviço falhou por causa de uma exceção do hibernate. */
 			throw new ServiceException(MessageList.createSingleInternalError(e));
 		}
