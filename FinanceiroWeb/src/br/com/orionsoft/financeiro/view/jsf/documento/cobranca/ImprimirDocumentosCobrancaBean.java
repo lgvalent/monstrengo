a7980df4ed5d8ffe41db4f1efbd7a0755c9126d6
Lucio Valentin <lgvalentin@utfpr.edu.br>
2020-09-25 14:41:23 -0300

    Opção de DownloadPdfZip para que os arquivos sairam separados

--- FinanceiroWeb/src/br/com/orionsoft/financeiro/view/jsf/documento/cobranca/ImprimirDocumentosCobrancaBean.java
+++ FinanceiroWeb/src/br/com/orionsoft/financeiro/view/jsf/documento/cobranca/ImprimirDocumentosCobrancaBean.java
@@ -58,15 +58,15 @@ public class ImprimirDocumentosCobrancaBean extends BeanSessionBasic implements 
 	public static final String VIEW_NAME = "imprimirDocumentosCobrancaBean";
 	public static final String FACES_VIEW_PASSO_1 = "/pages/financeiro/documentoCobrancaImprimir1?faces-redirect=true";
 	public static final String FACES_VIEW_PASSO_2 = "/pages/financeiro/documentoCobrancaImprimir2?faces-redirect=true";
-	
-    public static final String URL_PARAM_NOME_GERENCIADOR_DOCUMENTO = "nomeGerenciadorDocumento";
+
+	public static final String URL_PARAM_NOME_GERENCIADOR_DOCUMENTO = "nomeGerenciadorDocumento";
 
 	//  será feito o upload de um arquivoRetorno de retorno
-    private UploadedFile arquivoImagem;
-    private ImprimirDocumentosCobrancaProcess currentProcess = null;
+	private UploadedFile arquivoImagem;
+	private ImprimirDocumentosCobrancaProcess currentProcess = null;
 
-    public ImprimirDocumentosCobrancaProcess getCurrentProcess() {
-		
+	public ImprimirDocumentosCobrancaProcess getCurrentProcess() {
+
 		if (currentProcess == null) {
 			try {
 				currentProcess = (ImprimirDocumentosCobrancaProcess) this.getApplicationBean().getProcessManager().createProcessByName(ImprimirDocumentosCobrancaProcess.PROCESS_NAME, this.getUserSessionBean().getUserSession());
@@ -74,209 +74,230 @@ public class ImprimirDocumentosCobrancaBean extends BeanSessionBasic implements 
 				FacesUtils.addErrorMsgs(e.getErrorList());
 			}
 		}
-    	return currentProcess;
+		return currentProcess;
 	}
-    
+
 	public String actionStart() {
 		loadParams();
 
 		return FACES_VIEW_PASSO_1;
 	}
-    
-    /**
-     * Verifica se veio um parametro de filtro de nomeGerenciadorDocumento para listar somente
-     * documentos de um determinado gerenciador (GRCS, Titulos, etc)
-     */
+
+	/**
+	 * Verifica se veio um parametro de filtro de nomeGerenciadorDocumento para listar somente
+	 * documentos de um determinado gerenciador (GRCS, Titulos, etc)
+	 */
 	public void loadParams(){
-        if (FacesUtils.isNotNull(super.getRequestParams().get(URL_PARAM_NOME_GERENCIADOR_DOCUMENTO))){ 
-        	String nomeGerenciadorDocumento = super.getRequestParams().get(URL_PARAM_NOME_GERENCIADOR_DOCUMENTO).toString();
-        	
-    	    this.getCurrentProcess().setNomeGerenciadorDocumento(nomeGerenciadorDocumento);
-        }
-    }
+		if (FacesUtils.isNotNull(super.getRequestParams().get(URL_PARAM_NOME_GERENCIADOR_DOCUMENTO))){ 
+			String nomeGerenciadorDocumento = super.getRequestParams().get(URL_PARAM_NOME_GERENCIADOR_DOCUMENTO).toString();
 
-    /**
-     * Este método retorna uma String para o FacesUtils 
-     * redirecionar para a proxima tela, onde estará a visualização
-     *
-     */
-    public String doVisualizar(){
-    	/* Os parâmetros são inseridos diretamente no processo
-    	 * pelo Faces */
-    	String result = null;
-    	if(getCurrentProcess().runVisualizar()){
+			this.getCurrentProcess().setNomeGerenciadorDocumento(nomeGerenciadorDocumento);
+		}
+	}
+
+	/**
+	 * Este método retorna uma String para o FacesUtils 
+	 * redirecionar para a proxima tela, onde estará a visualização
+	 *
+	 */
+	public String doVisualizar(){
+		/* Os parâmetros são inseridos diretamente no processo
+		 * pelo Faces */
+		String result = null;
+		if(getCurrentProcess().runVisualizar()){
 			FacesUtils.addInfoMsgs(getCurrentProcess().getMessageList());
-   			result = FACES_VIEW_PASSO_2;
-    	}
-    	else{
-    		FacesUtils.addErrorMsgs(getCurrentProcess().getMessageList());
-    		result = FACES_VIEW_PASSO_1;
-    	}
-    	
-    	return result;
-    }
+			result = FACES_VIEW_PASSO_2;
+		}
+		else{
+			FacesUtils.addErrorMsgs(getCurrentProcess().getMessageList());
+			result = FACES_VIEW_PASSO_1;
+		}
 
-    public String doVisualizarEtiquetas(){
-    	return LabelBean.FACES_VIEW_LABELS;
-    }
+		return result;
+	}
 
-    
-    /**
-     * Esta ação gera o PDF do documento
-     * @return
-     * @throws ParseException
-     * @throws BusinessException
-     */
-    public void doDownload() throws ParseException, BusinessException {
-        log.debug("ImprimirDocumentoCobrancaBean.doDownload");
+	public String doVisualizarEtiquetas(){
+		return LabelBean.FACES_VIEW_LABELS;
+	}
 
-        /* Permite acionar esta action de tela passando somente o documentoId pela URL */
-       	this.loadParams();
-       	
-       	try {
-       		/* Define o outputStream */
-       		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-   			response.setContentType("pdf-content");
-       		response.setHeader("Content-Disposition", "attachment;filename=\"" + prepareFileName() + ".pdf\"");
 
-       		ServletOutputStream out = response.getOutputStream();
-       		
-       		/* Define o outputStream que receberá o arquivo pdf do relatório
-       		 * E anula o índice da impressora */
-      		this.getCurrentProcess().setEnviarEMail(false);
-       		this.getCurrentProcess().setOutputStream(out);
-       		this.getCurrentProcess().setPrinterIndex(PrintUtils.PRINTER_INDEX_NO_PRINT);
-       		this.getCurrentProcess().setInputStreamImagem(arquivoImagem==null?null:arquivoImagem.getInputstream());
+	/**
+	 * Esta ação gera o PDF do documento
+	 * @return
+	 * @throws ParseException
+	 * @throws BusinessException
+	 */
+	public void doDownload() throws ParseException, BusinessException {
+		log.debug("ImprimirDocumentoCobrancaBean.doDownload");
 
-       		if (this.getCurrentProcess().runImprimir()){
-       			/* Adiciona as mensagens de info no Faces */
-       			FacesUtils.addInfoMsgs(this.getCurrentProcess().getMessageList());
+		/* Permite acionar esta action de tela passando somente o documentoId pela URL */
+		this.loadParams();
 
-       		}else{
-       			/* Adiciona as mensagens de erro no Faces */
-       			FacesUtils.addErrorMsgs(this.getCurrentProcess().getMessageList());
-       			response.setContentType("text/html");
-           		response.setHeader("Content-Disposition", "filename=\"error.html\"");
+		try {
+			/* Define o outputStream */
+			HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+			if(this.currentProcess.getDownloadPdfZip()) {
+				response.setContentType("application/zip");
+				response.setHeader("Content-Disposition", "attachment;filename=\"" + prepareFileName() + ".zip\"");
+			}else {
+				response.setContentType("pdf-content");
+				response.setHeader("Content-Disposition", "attachment;filename=\"" + prepareFileName() + ".pdf\"");
+			}
 
-       			out.print(this.getCurrentProcess().getMessageList().get(0).getMessage());
-       		}
+			ServletOutputStream out = response.getOutputStream();
+
+			/* Define o outputStream que receberá o arquivo pdf do relatório
+			 * E anula o índice da impressora */
+			this.getCurrentProcess().setEnviarEMail(false);
+			this.getCurrentProcess().setOutputStream(out);
+			this.getCurrentProcess().setPrinterIndex(PrintUtils.PRINTER_INDEX_NO_PRINT);
+			this.getCurrentProcess().setInputStreamImagem(arquivoImagem==null?null:arquivoImagem.getInputstream());
+
+			if (this.getCurrentProcess().runImprimir()){
+				/* Adiciona as mensagens de info no Faces */
+				FacesUtils.addInfoMsgs(this.getCurrentProcess().getMessageList());
+
+			}else{
+				/* Adiciona as mensagens de erro no Faces */
+				FacesUtils.addErrorMsgs(this.getCurrentProcess().getMessageList());
+				response.setContentType("text/html");
+				response.setHeader("Content-Disposition", "filename=\"error.html\"");
+
+				out.print(this.getCurrentProcess().getMessageList().get(0).getMessage());
+			}
 			out.flush();
 			out.close();
 			FacesContext.getCurrentInstance().responseComplete();
-       	} catch (IOException e) {
-       		FacesUtils.addErrorMsg(e.getMessage());
+		} catch (IOException e) {
+			FacesUtils.addErrorMsg(e.getMessage());
 		}
-    }
-    
-    /**
-     * Efetua o download do documento em PDF
-     */
-    public void doEnviarEMail() {
-       	try {
-       		/* Informa ao processo para enviar e-mail */
-       		this.getCurrentProcess().setEnviarEMail(true);
-       	    this.getCurrentProcess().setOutputStream(null);
-       		this.getCurrentProcess().setPrinterIndex(PrintUtils.PRINTER_INDEX_NO_PRINT);
-       		this.getCurrentProcess().setInputStreamImagem(arquivoImagem==null?null:arquivoImagem.getInputstream());
+	}
 
-       		if (this.getCurrentProcess().runImprimir()){
-           		/* Adiciona as mensagens de info no Faces */
-       			FacesUtils.addInfoMsgs(this.getCurrentProcess().getMessageList());
-       		}else{
-       			/* Adiciona as mensagens de erro no Faces */
-       			FacesUtils.addErrorMsgs(this.getCurrentProcess().getMessageList());
-       		}
-       	} catch (IOException e) {
-       		FacesUtils.addErrorMsg(e.getMessage());
+
+	/**
+	 * Efetua o download do documento em PDF separados em ZIP
+	 */
+	public void doDownloadPdfZip() {
+		try {
+			/* Informa ao processo para enviar e-mail */
+			this.getCurrentProcess().setEnviarEMail(false);
+			this.getCurrentProcess().setDownloadPdfZip(true);
+			this.doDownload();
+		} catch (Exception e) {
+			FacesUtils.addErrorMsg(e.getMessage());
 		}
-    }
-    
-    /**
-     * Esta ação imprime o documento na impressora definida no printerIndex
-     * atual do processo
-     * @return
-     * @throws ParseException
-     * @throws BusinessException
-     */
-    public void doImprimir() throws BusinessException {
-    	log.debug("ImprimirDocumentoCobrancaBean.doImprimir");
 
-    	try{
-    		/* O printerIndex é injetado diretamente pelo JSF
-    		 * E anula o outputStream */
-    		//this.getProcess().setPrinterIndex(printerIndex);
-      		this.getCurrentProcess().setEnviarEMail(false);
-    		this.getCurrentProcess().setOutputStream(null);
-       		this.getCurrentProcess().setInputStreamImagem(arquivoImagem==null?null:arquivoImagem.getInputstream());
+	}
 
-    		if (this.getCurrentProcess().runImprimir()){
-    			/* Adiciona as mensagens de info no Faces */
-    			FacesUtils.addInfoMsgs(this.getCurrentProcess().getMessageList());
+	/**
+	 * Efetua o download do documento em PDF
+	 */
+	public void doEnviarEMail() {
+		try {
+			/* Informa ao processo para enviar e-mail */
+			this.getCurrentProcess().setEnviarEMail(true);
+			this.getCurrentProcess().setOutputStream(null);
+			this.getCurrentProcess().setPrinterIndex(PrintUtils.PRINTER_INDEX_NO_PRINT);
+			this.getCurrentProcess().setInputStreamImagem(arquivoImagem==null?null:arquivoImagem.getInputstream());
 
-    		}else{
-    			/* Adiciona as mensagens de erro no Faces */
-    			FacesUtils.addErrorMsgs(this.getCurrentProcess().getMessageList());
-    		}
-    	} catch (IOException e) {
-    		FacesUtils.addErrorMsg(e.getMessage());
-    	}
-    }
+			if (this.getCurrentProcess().runImprimir()){
+				/* Adiciona as mensagens de info no Faces */
+				FacesUtils.addInfoMsgs(this.getCurrentProcess().getMessageList());
+			}else{
+				/* Adiciona as mensagens de erro no Faces */
+				FacesUtils.addErrorMsgs(this.getCurrentProcess().getMessageList());
+			}
+		} catch (IOException e) {
+			FacesUtils.addErrorMsg(e.getMessage());
+		}
+	}
 
-    
-    private String prepareFileName() {
-    	String result = "Doc ";
-    	
-    	/* Vencimento !!!!!!!!! */
-    	result += CalendarUtils.formatDate(getCurrentProcess().getDataDocumentoDe()) + " a " + CalendarUtils.formatDate(getCurrentProcess().getDataDocumentoAte());
-    	result += " - Vencimento " + CalendarUtils.formatDate(getCurrentProcess().getDataVencimentoDe()) + " a " + CalendarUtils.formatDate(getCurrentProcess().getDataVencimentoAte());
-    	
-    	/* Se tiver CNPJ simplesmente exibe o CNPJ no nome do PDF */
-    	if(StringUtils.isNotEmpty(getCurrentProcess().getCpfCnpj()))
-    		result += "-" + getCurrentProcess().getCpfCnpj();
-    	else{
-        	/* Senão exibe as informações definidas no filtro e o número de guias que contém o arquivo */
-    		if(!getCurrentProcess().getParamEscritorioContabil().isNull())
-        		result += " - Escritório " + getCurrentProcess().getParamEscritorioContabil().getValue();
-    		if(!getCurrentProcess().getParamMunicipio().isNull())
-        		result += " - Município " + getCurrentProcess().getParamMunicipio().getValue();
-    		if(getCurrentProcess().getExcluirMunicipio() != null && getCurrentProcess().getExcluirMunicipio())
-        		result += " (Omitir)";
-    		result += " - " + getCurrentProcess().getSize() + " guia(s)";
-    	}
-    	
-    	/* Substitui a / pelo - no nome do arquivo */
-    	return result.replace("/", "-");
-    }
-    
-    public void doGerarEtiquetas() {
-    	if (getCurrentProcess().runGerarEtiquetas()) {
-    		/* Definir as mensagens a serem exibidas na pagina */
-    		FacesUtils.addInfoMsg("");//O menuLayout mostra a primeira mensagem contida no messagesBean
-    		FacesUtils.addInfoMsg("<hr>");
-    		FacesUtils.addInfoMsgs(getCurrentProcess().getMessageList());
-    	} else {
-    		FacesUtils.addErrorMsg("Ocorreram erros durante a geração de etiqueta.");
-    		FacesUtils.addErrorMsgs(getCurrentProcess().getMessageList());
-    		/* Definir o fluxo de tela de FAILURE */
-    	}    
-    }
-    
-    /**
-     * Este metodo faz a conclusão do processo. 
-     * Quando chamado pela interface, fecha a tela corrente.
-     */
-    public String doConcluir() {
-    	if (currentProcess != null) {
-    		try {
+	/**
+	 * Esta ação imprime o documento na impressora definida no printerIndex
+	 * atual do processo
+	 * @return
+	 * @throws ParseException
+	 * @throws BusinessException
+	 */
+	public void doImprimir() throws BusinessException {
+		log.debug("ImprimirDocumentoCobrancaBean.doImprimir");
+
+		try{
+			/* O printerIndex é injetado diretamente pelo JSF
+			 * E anula o outputStream */
+			//this.getProcess().setPrinterIndex(printerIndex);
+			this.getCurrentProcess().setEnviarEMail(false);
+			this.getCurrentProcess().setOutputStream(null);
+			this.getCurrentProcess().setInputStreamImagem(arquivoImagem==null?null:arquivoImagem.getInputstream());
+
+			if (this.getCurrentProcess().runImprimir()){
+				/* Adiciona as mensagens de info no Faces */
+				FacesUtils.addInfoMsgs(this.getCurrentProcess().getMessageList());
+
+			}else{
+				/* Adiciona as mensagens de erro no Faces */
+				FacesUtils.addErrorMsgs(this.getCurrentProcess().getMessageList());
+			}
+		} catch (IOException e) {
+			FacesUtils.addErrorMsg(e.getMessage());
+		}
+	}
+
+
+	private String prepareFileName() {
+		String result = "Doc ";
+
+		/* Vencimento !!!!!!!!! */
+		result += CalendarUtils.formatDate(getCurrentProcess().getDataDocumentoDe()) + " a " + CalendarUtils.formatDate(getCurrentProcess().getDataDocumentoAte());
+		result += " - Vencimento " + CalendarUtils.formatDate(getCurrentProcess().getDataVencimentoDe()) + " a " + CalendarUtils.formatDate(getCurrentProcess().getDataVencimentoAte());
+
+		/* Se tiver CNPJ simplesmente exibe o CNPJ no nome do PDF */
+		if(StringUtils.isNotEmpty(getCurrentProcess().getCpfCnpj()))
+			result += "-" + getCurrentProcess().getCpfCnpj();
+		else{
+			/* Senão exibe as informações definidas no filtro e o número de guias que contém o arquivo */
+			if(!getCurrentProcess().getParamEscritorioContabil().isNull())
+				result += " - Escritório " + getCurrentProcess().getParamEscritorioContabil().getValue();
+			if(!getCurrentProcess().getParamMunicipio().isNull())
+				result += " - Município " + getCurrentProcess().getParamMunicipio().getValue();
+			if(getCurrentProcess().getExcluirMunicipio() != null && getCurrentProcess().getExcluirMunicipio())
+				result += " (Omitir)";
+			result += " - " + getCurrentProcess().getSize() + " guia(s)";
+		}
+
+		/* Substitui a / pelo - no nome do arquivo */
+		return result.replace("/", "-");
+	}
+
+	public void doGerarEtiquetas() {
+		if (getCurrentProcess().runGerarEtiquetas()) {
+			/* Definir as mensagens a serem exibidas na pagina */
+			FacesUtils.addInfoMsg("");//O menuLayout mostra a primeira mensagem contida no messagesBean
+			FacesUtils.addInfoMsg("<hr>");
+			FacesUtils.addInfoMsgs(getCurrentProcess().getMessageList());
+		} else {
+			FacesUtils.addErrorMsg("Ocorreram erros durante a geração de etiqueta.");
+			FacesUtils.addErrorMsgs(getCurrentProcess().getMessageList());
+			/* Definir o fluxo de tela de FAILURE */
+		}    
+	}
+
+	/**
+	 * Este metodo faz a conclusão do processo. 
+	 * Quando chamado pela interface, fecha a tela corrente.
+	 */
+	public String doConcluir() {
+		if (currentProcess != null) {
+			try {
 				currentProcess.finish();
 			} catch (ProcessException e) {
 				FacesUtils.addErrorMsgs(e.getErrorList());
 			}
-    		currentProcess = null;
-    	}
-    	return FacesUtils.FACES_VIEW_CLOSE;
-    }
-    
+			currentProcess = null;
+		}
+		return FacesUtils.FACES_VIEW_CLOSE;
+	}
+
 	public void doReset() throws BusinessException, Exception
 	{
 	}
@@ -284,38 +305,38 @@ public class ImprimirDocumentosCobrancaBean extends BeanSessionBasic implements 
 	public void doReload() throws BusinessException, Exception
 	{
 	}
-	
+
 	public UploadedFile getArquivoImagem() {return arquivoImagem;}
 	public void setArquivoImagem(UploadedFile arquivoImagem) {this.arquivoImagem = arquivoImagem;}
-	
-	
+
+
 
 	@SuppressWarnings("unchecked")
 	public String runWithEntities(IEntityCollection<?> entities) {
-	        List<DocumentoCobrancaBean> docs = new ArrayList<DocumentoCobrancaBean>(entities.getSize());
+		List<DocumentoCobrancaBean> docs = new ArrayList<DocumentoCobrancaBean>(entities.getSize());
 
-	        /* Converte a lista de IEntity em beans */
-	        for(IEntity<?> entitiy: entities){
-	    		if (ClassUtils.isAssignable(entities.getInfo().getType(), DocumentoCobranca.class)){
-	    			docs.add(new DocumentoCobrancaBean((IEntity<? extends DocumentoCobranca>) entitiy, ""));
-	    		}
-	        	else if (ClassUtils.isAssignable(entities.getInfo().getType(), Lancamento.class)){
-	        		IEntity<Lancamento> lan = (IEntity<Lancamento>) entitiy;
-	        		/* verifica se tem documento de cobrança neste lançamento */
-	        		try {
-						entitiy = (IEntity<?>) lan.getPropertyValue(Lancamento.DOCUMENTO_COBRANCA);
-					} catch (EntityException e) {
-						FacesUtils.addErrorMsgs(e.getErrorList());
-						return FacesUtils.FACES_VIEW_FAILURE;
-					}
-	        		if(entitiy != null)
-	        			docs.add(new DocumentoCobrancaBean((IEntity<? extends DocumentoCobranca>) entitiy, ""));
-	        	}
-	        }
-	        	
-	        /* Envia os beans para o processo de impressão de documentos */
-	        this.getCurrentProcess().setBeanList(docs);
-			
+		/* Converte a lista de IEntity em beans */
+		for(IEntity<?> entitiy: entities){
+			if (ClassUtils.isAssignable(entities.getInfo().getType(), DocumentoCobranca.class)){
+				docs.add(new DocumentoCobrancaBean((IEntity<? extends DocumentoCobranca>) entitiy, ""));
+			}
+			else if (ClassUtils.isAssignable(entities.getInfo().getType(), Lancamento.class)){
+				IEntity<Lancamento> lan = (IEntity<Lancamento>) entitiy;
+				/* verifica se tem documento de cobrança neste lançamento */
+				try {
+					entitiy = (IEntity<?>) lan.getPropertyValue(Lancamento.DOCUMENTO_COBRANCA);
+				} catch (EntityException e) {
+					FacesUtils.addErrorMsgs(e.getErrorList());
+					return FacesUtils.FACES_VIEW_FAILURE;
+				}
+				if(entitiy != null)
+					docs.add(new DocumentoCobrancaBean((IEntity<? extends DocumentoCobranca>) entitiy, ""));
+			}
+		}
+
+		/* Envia os beans para o processo de impressão de documentos */
+		this.getCurrentProcess().setBeanList(docs);
+
 		return FACES_VIEW_PASSO_2;
 	}
 
@@ -336,6 +357,6 @@ public class ImprimirDocumentosCobrancaBean extends BeanSessionBasic implements 
 
 		return FACES_VIEW_PASSO_1;
 	}
-	
-	
+
+
 }
